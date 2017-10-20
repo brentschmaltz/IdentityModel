c43a2677eba4da158c01f5c1235bdc07d9799076
BrentSchmaltz <brentschmaltz@hotmail.com>
2017-10-20 13:20:23 -0700

    added example of JWE.

--- src/CreateTokens/CreateTokens/Program.cs
+++ src/CreateTokens/CreateTokens/Program.cs
@@ -13,6 +13,7 @@ namespace CreateTokens
     {
         static void Main(string[] args)
         {
+            // some local variables for all cases
             var audience = "microsoft.com";
             var issuer = "contoso.com";
             var subject = new ClaimsIdentity(new List<Claim>
@@ -23,23 +24,41 @@ namespace CreateTokens
 
             var tokenHandler = new JwtSecurityTokenHandler();
 
-            // symmetric key
-            var encodedJwtSymmetric = CreateEncodedToken(subject, audience, issuer, tokenHandler, new SigningCredentials(KeyMaterial.SymmetricSecurityKey_256, SecurityAlgorithms.HmacSha256));
-            var claimsPrincipalSymmetric = ValidateToken(encodedJwtSymmetric, tokenHandler, audience, issuer, KeyMaterial.SymmetricSecurityKey_256);
+            // Create and validate a Compact JWS see: https://tools.ietf.org/html/rfc7515#section-3.1
 
-            // asymmetric key
-            var encodedJwtAsymmetric = CreateEncodedToken(subject, audience, issuer, tokenHandler, new SigningCredentials(KeyMaterial.RsaSecurityKey_2048, SecurityAlgorithms.RsaSha256));
-            var claimsPrincipalAsymmetric = ValidateToken(encodedJwtAsymmetric, tokenHandler, audience, issuer, KeyMaterial.RsaSecurityKey_2048);
+            // sign with shared symmetric key
+            var symmetricSigningCredentials = new SigningCredentials(KeyMaterial.SymmetricSecurityKey_256, SecurityAlgorithms.HmacSha256);
+            var jwtSymmetric = CreateEncodedJwt(subject, audience, issuer, tokenHandler, symmetricSigningCredentials, null);
+            var claimsPrincipalSymmetric = ValidateToken(jwtSymmetric, tokenHandler, audience, issuer, KeyMaterial.SymmetricSecurityKey_256, null);
 
+            // sign with asymmetric key that has private key, validate with public key
+            var asymmetricSigningCredentials = new SigningCredentials(KeyMaterial.RsaSecurityKey_2048, SecurityAlgorithms.RsaSha256);
+            var jwtAsymmetric = CreateEncodedJwt(subject, audience, issuer, tokenHandler, asymmetricSigningCredentials, null);
+            var claimsPrincipalAsymmetric = ValidateToken(jwtAsymmetric, tokenHandler, audience, issuer, KeyMaterial.RsaSecurityKey_2048_Public, null);
+
+            // Create and validate a JWE see: https://tools.ietf.org/html/rfc7516#section-3
+
+            // encrypt with a shared symmetric key to wrap the key that encrypts the payload
+            var symmetricEncryptionCredentials = new EncryptingCredentials(KeyMaterial.SymmetricSecurityKey_256, "dir", SecurityAlgorithms.Aes128CbcHmacSha256);
+            var jweSymmetric = CreateEncodedJwt(subject, audience, issuer, tokenHandler, asymmetricSigningCredentials, symmetricEncryptionCredentials);
+            var claimsPrincipalJweSymmetric = ValidateToken(jweSymmetric, tokenHandler, audience, issuer, KeyMaterial.RsaSecurityKey_2048_Public, KeyMaterial.SymmetricSecurityKey_256);
+
+            // encrypt with recipients public key to wrap a key that encrypts the payload. Decrypt using pirvate key.
+            var asymmetricEncryptionCredentials = new EncryptingCredentials(KeyMaterial.RsaSecurityKey_2048_Public, SecurityAlgorithms.RsaOAEP, SecurityAlgorithms.Aes128CbcHmacSha256);
+            var jweAymmetric = CreateEncodedJwt(subject, audience, issuer, tokenHandler, symmetricSigningCredentials, asymmetricEncryptionCredentials);
+            var claimsPrincipalJweAsymmetric = ValidateToken(jweAymmetric, tokenHandler, audience, issuer, KeyMaterial.SymmetricSecurityKey_256, KeyMaterial.RsaSecurityKey_2048);
+
+            Console.WriteLine("CreateTokens completed. If no exceptions, all went well.");
             Console.WriteLine("Press any key to close");
             Console.ReadKey();
         }
 
-        private static string CreateEncodedToken(ClaimsIdentity subject, string audience, string issuer, JwtSecurityTokenHandler tokenHandler, SigningCredentials signingCredentials)
+        private static string CreateEncodedJwt(ClaimsIdentity subject, string audience, string issuer, JwtSecurityTokenHandler tokenHandler, SigningCredentials signingCredentials, EncryptingCredentials encryptingCredentials)
         {
             var tokenDescriptor = new SecurityTokenDescriptor()
             {
                 Audience = audience,
+                EncryptingCredentials = encryptingCredentials,
                 Issuer = issuer,
                 SigningCredentials = signingCredentials,
                 Subject = subject
@@ -57,19 +76,19 @@ namespace CreateTokens
             return null;
         }
 
-        private static ClaimsPrincipal ValidateToken(string encodedJwt, JwtSecurityTokenHandler tokenHandler, string audience, string issuer,  SecurityKey securityKey)
+        private static ClaimsPrincipal ValidateToken(string encodedJwt, JwtSecurityTokenHandler tokenHandler, string audience, string issuer, SecurityKey signingKey, SecurityKey encryptionKey)
         {
             var validationParameters = new TokenValidationParameters
             {
                 ValidAudience = audience,
                 ValidIssuer = issuer,
-                IssuerSigningKey = securityKey
+                IssuerSigningKey = signingKey,
+                TokenDecryptionKey = encryptionKey
             };
 
-            SecurityToken securityToken = null;
             try
             {
-                return tokenHandler.ValidateToken(encodedJwt, validationParameters, out securityToken);
+                return tokenHandler.ValidateToken(encodedJwt, validationParameters, out SecurityToken securityToken);
             }
             catch (Exception ex)
             {
